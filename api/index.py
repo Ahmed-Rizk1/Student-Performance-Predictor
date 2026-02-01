5c1eb9694648b487bf3e26cecd9fbb3dbd63ff8a
Ahmed-Rizk1 <ahmedrizkm681@gmail.com>
2026-02-01 20:20:45 +0200

    fix: Retrain model with dill protocol 4 and add error logging

--- api/index.py
+++ api/index.py
@@ -17,23 +17,30 @@ def index():
 
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
-        )
-        pred_df = data.get_data_as_data_frame()
-        
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+    try:
+        if request.method == 'GET':
+            return render_template('home.html')
+        else:
+            data = CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('reading_score')),
+                writing_score=float(request.form.get('writing_score'))
+            )
+            pred_df = data.get_data_as_data_frame()
+            print("Before Prediction")
+            
+            predict_pipeline = PredictPipeline()
+            print("Mid Prediction")
+            results = predict_pipeline.predict(pred_df)
+            print("after Prediction")
+            return render_template('home.html', results=results[0])
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return f"An error occurred: {e}", 500
 
 # Vercel serverless handler
 app = app
